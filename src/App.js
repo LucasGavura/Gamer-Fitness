e7e8ccfbbc00c622282abb16454e3fec98ff197a
Lucas Gavura <lgavu074@uottawa.ca>
2024-07-07 19:00:45 -0400

    adding communication

--- src/App.js
+++ src/App.js
@@ -6,30 +6,34 @@ import Divergence from './components/Divergence';
 import Absorb from './components/Absorb';
 import Monitored from './components/Monitored';
 import Organize from './components/Organize';
+import Chat from './components/Chat';
 import './App.css';
 
 
 function App() {
   return (
     <div className="App">
-      <div className="container">
-        <Navbar />
-      </div>
-      <div className="container">
-        <Home />
-      </div>
-      <div className="container">
-        <Divergence />
-      </div>
-      <div className="container">
-        <Absorb />
-      </div>
-      <div className="container">
-        <Monitored />
-      </div>
-      <div className="container">
-        <Organize />
-      </div>
+      <main>
+        <div className="container">
+          <Navbar />
+        </div>
+        <div className="container">
+          <Home />
+        </div>
+        <div className="container">
+          <Divergence />
+        </div>
+        <div className="container">
+          <Absorb />
+        </div>
+        <div className="container">
+          <Monitored />
+        </div>
+        <div className="container">
+          <Organize />
+        </div>
+      </main>
+      <Chat />
     </div>
   );
 }
