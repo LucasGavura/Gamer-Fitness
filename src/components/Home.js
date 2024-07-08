dbf1a4561517d5bb371ff1096533b0de24b4b0af
Lucas Gavura <lgavu074@uottawa.ca>
2024-07-07 21:47:29 -0400

    proper scrolling

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,15 +1,20 @@
+// src/components/Home.js
 import React from 'react';
 import './Home.css';
 import homeImage from '../images/Home.png';
 
 const Home = () => {
+  const scrollToDivergence = () => {
+    const element = document.getElementById('divergence');
+    if (element) {
+      element.scrollIntoView({ behavior: 'smooth' });
+    }
+  };
   return (
     <section id="home" className="home" style={{ backgroundImage: `url(${homeImage})` }}>
       <h1>Your Journey Starts Here</h1>
       <p>Welcome to Gamer Fitness</p>
-      <ScrollLink to="divergence" smooth={true} duration={500}>
-        <button className="home-button">Get Started</button>
-      </ScrollLink>
+      <button onClick={scrollToDivergence} className="home-button">Get Started</button>
     </section>
   );
 };
